92ba4c79e84fabf90b7519f8063b94d2a60b451e
Jordan-Gong <jordan_gong@outlook.com>
2020-01-03 20:24:33 +0800

    Add GPL license

--- translate/zh_CN.ts
+++ translate/zh_CN.ts
@@ -3,147 +3,147 @@
 <context>
     <name>ApplicationWindow</name>
     <message>
-        <location filename="../main_intl.py" line="114"/>
+        <location filename="../main.py" line="130"/>
         <source>Import complete</source>
         <translation>导入完毕</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="116"/>
+        <location filename="../main.py" line="132"/>
         <source>Data has been imported successfully.</source>
         <translation>已成功导入数据。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="122"/>
+        <location filename="../main.py" line="138"/>
         <source>Import error</source>
         <translation>导入错误</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="124"/>
+        <location filename="../main.py" line="140"/>
         <source>Data might have wrong format</source>
         <translation>数据的格式可能有误</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="125"/>
+        <location filename="../main.py" line="141"/>
         <source>Please make sure the directory contains subfolders which are names of categories, and all images should be in subfolders.</source>
         <translation>请确保包含的子文件夹的文件夹名是图像的类别，所有的图像分散在子文件夹中。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="136"/>
+        <location filename="../main.py" line="152"/>
         <source>Load model</source>
         <translation>加载模型</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="181"/>
+        <location filename="../main.py" line="197"/>
         <source>Joblib dump file </source>
         <translation>Joblib 内存转储文件 </translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="158"/>
+        <location filename="../main.py" line="174"/>
         <source>Loading complete</source>
         <translation>加载完毕</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="160"/>
+        <location filename="../main.py" line="176"/>
         <source>Model has been loaded successfully.</source>
         <translation>已成功加载模型</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="173"/>
+        <location filename="../main.py" line="189"/>
         <source>Loading error</source>
         <translation>加载错误</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="169"/>
+        <location filename="../main.py" line="185"/>
         <source>This might not be a joblib dump file.</source>
         <translation>该文件可能不是 joblib 内存转储文件。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="175"/>
+        <location filename="../main.py" line="191"/>
         <source>This file is not a instance of RBF-SVM.</source>
         <translation>该文件不是 RBF-SVM 的实例。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="181"/>
+        <location filename="../main.py" line="197"/>
         <source>Save model</source>
         <translation>保存模型</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="191"/>
+        <location filename="../main.py" line="207"/>
         <source>Save complete</source>
         <translation>保存完毕</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="193"/>
+        <location filename="../main.py" line="209"/>
         <source>Model has been saved successfully.</source>
         <translation>已成功保存模型。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="234"/>
+        <location filename="../main.py" line="250"/>
         <source>Training complete</source>
         <translation>训练完成</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="236"/>
+        <location filename="../main.py" line="252"/>
         <source>Data has been trained successfully.</source>
         <translation>已成功训练数据。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="252"/>
+        <location filename="../main.py" line="268"/>
         <source>Mean test score</source>
         <translation>平均测试评分</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="252"/>
+        <location filename="../main.py" line="268"/>
         <source>Std test score</source>
         <translation>测试评分标准差</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="252"/>
+        <location filename="../main.py" line="268"/>
         <source>Rank test score</source>
         <translation>测试评分排名</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="253"/>
+        <location filename="../main.py" line="269"/>
         <source>Mean fit time</source>
         <translation>平均训练时间</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="253"/>
+        <location filename="../main.py" line="269"/>
         <source>Std fit time</source>
         <translation>训练时间标准差</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="282"/>
+        <location filename="../main.py" line="298"/>
         <source>Load and Predict</source>
         <translation>加载并预测</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="293"/>
+        <location filename="../main.py" line="309"/>
         <source>Choose image directory</source>
         <translation>选择图像路径</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="324"/>
+        <location filename="../main.py" line="340"/>
         <source>Classification report</source>
         <translation>分类报告</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="336"/>
+        <location filename="../main.py" line="352"/>
         <source>Confusion matrix</source>
         <translation>混淆矩阵</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="348"/>
+        <location filename="../main.py" line="364"/>
         <source>ROC curve</source>
         <translation>ROC 曲线</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="388"/>
+        <location filename="../main.py" line="404"/>
         <source>Cross validation has been done successfully.</source>
         <translation>已成功完成交叉验证。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="397"/>
+        <location filename="../main.py" line="413"/>
         <source>Cross validation result</source>
         <translation>交叉验证结果</translation>
     </message>
@@ -158,92 +158,92 @@
         <translation type="obsolete">加载并预测</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="425"/>
+        <location filename="../main.py" line="441"/>
         <source>Import test images...</source>
         <translation>导入测试图片...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="485"/>
+        <location filename="../main.py" line="501"/>
         <source>Shuffle</source>
         <translation>看看别的</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="275"/>
+        <location filename="../main.py" line="291"/>
         <source>Best parameters:</source>
         <translation>最佳参数:</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="408"/>
+        <location filename="../main.py" line="424"/>
         <source>Current parameters:</source>
         <translation>当前参数:</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="67"/>
+        <location filename="../main.py" line="83"/>
         <source>Import training images...</source>
         <translation>导入训练图片...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="69"/>
+        <location filename="../main.py" line="85"/>
         <source>Load model...</source>
         <translation>加载模型...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="71"/>
+        <location filename="../main.py" line="87"/>
         <source>Save model...</source>
         <translation>保存模型...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="74"/>
+        <location filename="../main.py" line="90"/>
         <source>Load example</source>
         <translation>加载样例</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="76"/>
+        <location filename="../main.py" line="92"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="79"/>
+        <location filename="../main.py" line="95"/>
         <source>File</source>
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="89"/>
+        <location filename="../main.py" line="105"/>
         <source>Auto tuning...</source>
         <translation>自动调优...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="92"/>
+        <location filename="../main.py" line="108"/>
         <source>Manual setting...</source>
         <translation>手动设置...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="96"/>
+        <location filename="../main.py" line="112"/>
         <source>Hyper-parameter</source>
         <translation>超参数</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="389"/>
+        <location filename="../main.py" line="405"/>
         <source>This operation takes</source>
         <translation>该操作用时</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="389"/>
+        <location filename="../main.py" line="405"/>
         <source>s</source>
         <translation>秒</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="257"/>
+        <location filename="../main.py" line="273"/>
         <source>Split </source>
         <translation>第 </translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="257"/>
+        <location filename="../main.py" line="273"/>
         <source> test score</source>
         <translation> 部分测试评分</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="386"/>
+        <location filename="../main.py" line="402"/>
         <source>Cross validate complete</source>
         <translation>交叉验证完毕</translation>
     </message>
@@ -251,77 +251,77 @@
 <context>
     <name>AutoTuning</name>
     <message>
-        <location filename="../main_intl.py" line="587"/>
+        <location filename="../main.py" line="603"/>
         <source>Choose auto tuning method and parameters</source>
         <translation>选择调优方法与参数</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="599"/>
+        <location filename="../main.py" line="615"/>
         <source>5 Folds</source>
         <translation>5 折</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="601"/>
+        <location filename="../main.py" line="617"/>
         <source>Cross validation</source>
         <translation>交叉验证</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="605"/>
+        <location filename="../main.py" line="621"/>
         <source>Gird search</source>
         <translation>网格搜索</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="609"/>
+        <location filename="../main.py" line="625"/>
         <source>Random search</source>
         <translation>随机搜索</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="615"/>
+        <location filename="../main.py" line="631"/>
         <source>Logarithmic scale</source>
         <translation>对数标度</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="619"/>
+        <location filename="../main.py" line="635"/>
         <source>Linear scale</source>
         <translation>线性标度</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="624"/>
+        <location filename="../main.py" line="640"/>
         <source>Tuning method</source>
         <translation>调优方式</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="629"/>
+        <location filename="../main.py" line="645"/>
         <source>Iterations</source>
         <translation>迭代次数</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="677"/>
+        <location filename="../main.py" line="693"/>
         <source>Number</source>
         <translation>数量</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="690"/>
+        <location filename="../main.py" line="706"/>
         <source>Hyper-parameters</source>
         <translation>超参数</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="692"/>
+        <location filename="../main.py" line="708"/>
         <source>Tune and Train</source>
         <translation>调优并训练</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="743"/>
+        <location filename="../main.py" line="759"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="745"/>
+        <location filename="../main.py" line="761"/>
         <source>Right bound must greater than left bound.</source>
         <translation>右端点必须大于左端点</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="735"/>
+        <location filename="../main.py" line="751"/>
         <source> Folds</source>
         <translation> 折</translation>
     </message>
@@ -329,67 +329,67 @@
 <context>
     <name>ImportTrainingImage</name>
     <message>
-        <location filename="../main_intl.py" line="511"/>
+        <location filename="../main.py" line="527"/>
         <source>Import training image</source>
         <translation>导入训练图片</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="519"/>
+        <location filename="../main.py" line="535"/>
         <source>Choose...</source>
         <translation>选择...</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="522"/>
+        <location filename="../main.py" line="538"/>
         <source>Directory</source>
         <translation>路径</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="535"/>
+        <location filename="../main.py" line="551"/>
         <source>Image dimension</source>
         <translation>图像维数</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="538"/>
+        <location filename="../main.py" line="554"/>
         <source>Grayscale</source>
         <translation>灰度</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="541"/>
+        <location filename="../main.py" line="557"/>
         <source>RGB</source>
         <translation>彩色</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="543"/>
+        <location filename="../main.py" line="559"/>
         <source>Color mode</source>
         <translation>色彩模式</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="545"/>
+        <location filename="../main.py" line="561"/>
         <source>No description.</source>
         <translation>没有描述。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="546"/>
+        <location filename="../main.py" line="562"/>
         <source>Description</source>
         <translation>描述</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="548"/>
+        <location filename="../main.py" line="564"/>
         <source>Import</source>
         <translation>导入</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="554"/>
+        <location filename="../main.py" line="570"/>
         <source>Choose image directory</source>
         <translation>选择图像路径</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="565"/>
+        <location filename="../main.py" line="581"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="567"/>
+        <location filename="../main.py" line="583"/>
         <source>Please choose directory</source>
         <translation>请选择路径</translation>
     </message>
@@ -397,42 +397,42 @@
 <context>
     <name>ManualSetting</name>
     <message>
-        <location filename="../main_intl.py" line="775"/>
+        <location filename="../main.py" line="791"/>
         <source>Manual setting</source>
         <translation>手动设置</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="787"/>
+        <location filename="../main.py" line="803"/>
         <source>5 Folds</source>
         <translation>5 折</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="789"/>
+        <location filename="../main.py" line="805"/>
         <source>Cross validation</source>
         <translation>交叉验证</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="803"/>
+        <location filename="../main.py" line="819"/>
         <source>Hyper-parameters</source>
         <translation>超参数</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="806"/>
+        <location filename="../main.py" line="822"/>
         <source>Cross Validate</source>
         <translation>交叉验证</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="829"/>
+        <location filename="../main.py" line="845"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="831"/>
+        <location filename="../main.py" line="847"/>
         <source>should be auto, scale or numeric.</source>
         <translation>应该是 auto，scale 或数字。</translation>
     </message>
     <message>
-        <location filename="../main_intl.py" line="814"/>
+        <location filename="../main.py" line="830"/>
         <source> Folds</source>
         <translation> 折</translation>
     </message>
